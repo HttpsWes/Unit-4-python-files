384a32b4760acfbba55ad4a714b4ccbb58693405
wowwowwes <wihezuo@gmail.com>
2022-11-14 10:46:59 -0500

    final changes

--- add.py
+++ add.py
@@ -1,14 +1,17 @@
 print("I can add numbers for you")
 
+#My X and Y variables define my input statment
+#if is important i add interger tags so my terminal can read the input as a number
+
 x = int(input("give me one number"))
 y = int(input("give me one number"))
 
 
-x = int(x)
-y = int(y)
 
 
 print("your result is ")
+
+#I use the + opperator to  add the input statment of x and y
 print(int(x)+int(y))
 
 
--- grades.py
+++ grades.py
@@ -1,9 +1,15 @@
+# X is my vairble that i used to store the nmber 100
 x = 100
+
+# input statment. I  included a int type so my code can by enter as a interger
 x=int(input("What is your GPA?"))
 
+
+# This is my if conditional statement the prints; If x is greater then or equal to 90 you will recive an A.
 if x >= 90:
     print("Good Job you get an A")
 
+# This is my else conditional statement that decides if my if statment above is not true then the following will happening
 elif x >= 80 and x <=89:
     print("You scored Well. You get an B")
 
@@ -11,8 +17,10 @@ elif x >= 70 and x <=79:
     print("You did a decent job. You get an C")
 
 elif x >= 60 and x <=69:
-    print("You try harder next time. You get an D")
+    print("try harder next time. You get an D")
+#  --------------------------------------------------------------------------------------------------------------------------
 
+# When my other if and elif conditional statements are not met then this will print. 
 else:
    
     print("You failed. You get an F")
