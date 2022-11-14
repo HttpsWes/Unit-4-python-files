2e78dbd457b59c8e575d0c7f54cb06e220435de6
wowwowwes <wihezuo@gmail.com>
2022-11-14 10:23:12 -0500

    Grade with python

---
+++ grades.py
@@ -0,0 +1,18 @@
+x = 100
+x=int(input("What is your GPA?"))
+
+if x >= 90:
+    print("Good Job you get an A")
+
+elif x >= 80 and x <=89:
+    print("You scored Well. You get an B")
+
+elif x >= 70 and x <=79:
+    print("You did a decent job. You get an C")
+
+elif x >= 60 and x <=69:
+    print("You try harder next time. You get an D")
+
+else:
+   
+    print("You failed. You get an F")
