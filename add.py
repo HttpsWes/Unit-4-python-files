adf2b931ebf02e97fe004cc47f736ba6b3a88b99
wowwowwes <wihezuo@gmail.com>
2022-11-14 09:55:37 -0500

    add

---
+++ add.py
@@ -0,0 +1,14 @@
+print("I can add numbers for you")
+
+x = int(input("give me one number"))
+y = int(input("give me one number"))
+
+
+x = int(x)
+y = int(y)
+
+
+print("your result is ")
+print(int(x)+int(y))
+
+
