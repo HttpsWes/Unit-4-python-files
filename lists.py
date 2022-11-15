8fe2c97997848f4c78fb6b3a38464a48ce9566bb
wowwowwes <wihezuo@gmail.com>
2022-11-15 10:35:24 -0500

    TIRED

---
+++ lists.py
@@ -0,0 +1,25 @@
+
+my_list = [ 'Ramen', 'Udon', 'Wonton' ]
+
+my_list[0] = 'Udon'
+
+print(my_list)
+
+
+del my_list[2]
+
+print(my_list)
+
+my_tuple = ("apple", "banana", "orange", "grape")
+
+
+my_tuple.append("peer")
+
+print(my_tuple)
+
+del my_tuple[2]
+
+my_tuple[0]='peaches'
+
+print(my_tuple)
+
---
+++ loops.py
@@ -0,0 +1,23 @@
+# x = ''
+
+# # while x != 'stop':
+# #     print('hello world')
+# #     x = input('Type stop to exit')
+
+
+# y = 1
+
+# while y <= 10:
+#     print(y)
+
+# y = y + 1
+
+# groceries  = ['apple' , 'banana', 'orange']
+
+# for z in groceries:
+#     print(z)
+
+# print(range(10))
+
+for i in range(10,0):
+    print(i)
