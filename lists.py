bd8abedf962079883afb54c7136ce3123b05d378
wowwowwes <wihezuo@gmail.com>
2022-11-15 10:56:39 -0500

    done

--- lists.py
+++ lists.py
@@ -1,18 +1,26 @@
 
+# variable used to define my list
 my_list = [ 'Ramen', 'Udon', 'Wonton' ]
 
+
+# I changed the postion of Udon in my list from 1 to 0
 my_list[0] = 'Udon'
 
 print(my_list)
 
-
+# With del command i can delete items onmy list
 del my_list[2]
+
+# i added items to my list
+my_list.append("tomato")
 
 print(my_list)
 
+# I created a tuple and added items to it
 my_tuple = ("apple", "banana", "orange", "grape")
 
-
+#   Since tuples are imutable they don't all for ou to make changes
+# i tried to append and del my list but tuple would not allow me
 my_tuple.append("peer")
 
 print(my_tuple)
---
+++ todo.py
@@ -0,0 +1,10 @@
+todos  = []
+
+x = input("What to add to list?")
+
+todos.append(x)
+
+
+
+print("Your todo list is")
+print(todos)
