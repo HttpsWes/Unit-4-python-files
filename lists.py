da34584d6cf9cf947d6f6ae1db76bce6386a53fc
wowwowwes <wihezuo@gmail.com>
2022-11-16 09:31:11 -0500

    List

--- lists.py
+++ lists.py
@@ -6,6 +6,8 @@ my_list = [ 'Ramen', 'Udon', 'Wonton' ]
 # I changed the postion of Udon in my list from 1 to 0
 my_list[0] = 'Udon'
 
+my_list[1] = 'Ramen'
+
 print(my_list)
 
 # With del command i can delete items onmy list
@@ -31,3 +33,29 @@ my_tuple[0]='peaches'
 
 print(my_tuple)
 
+# Len command allows me to see the number of items in m list
+print(len(my_list))
+
+print(my_list)
+
+# sort allows me to sort my list items in order
+my_list.sort()
+
+print(my_list)
+# reveerse allows me to reverse the order of the items on my list
+my_list.reverse()
+
+print(my_list)
+
+second_list= ['blue', 'red', 'green', 'black', 'white']
+
+# i used the + opperator to combine my list
+print(my_list + second_list)
+
+# Used "join" to add a character between my items
+print(" | ".join(my_list + second_list))
+
+# split allows me to spereate stringes
+names = "anna, wesley, saint".split(" , ")
+
+print(names)
--- loops.py
+++ loops.py
@@ -1,23 +1,23 @@
-# x = ''
+x = ''
 
-# # while x != 'stop':
-# #     print('hello world')
-# #     x = input('Type stop to exit')
+while x != 'stop':
+    print('hello world')
+    x = input('Type stop to exit')
 
 
-# y = 1
+y = 1
 
-# while y <= 10:
-#     print(y)
+while y <= 10:
+    print(y)
 
-# y = y + 1
+y = y + 1
 
-# groceries  = ['apple' , 'banana', 'orange']
+groceries  = ['apple' , 'banana', 'orange']
 
-# for z in groceries:
-#     print(z)
+for z in groceries:
+    print(z)
 
-# print(range(10))
+print(range(10))
 
 for i in range(10,0):
     print(i)
