0b02b95da10ad484508671fc0dc2624296f4c198
wowwowwes <wihezuo@gmail.com>
2022-11-16 10:16:56 -0500

    functions

---
+++ functions.py
@@ -0,0 +1,46 @@
+
+#Created a function and added print statement to it
+def  hello_world():
+    print('hello world')
+
+hello_world()
+
+#Add
+def  add(a,b):
+    print(a+b)
+
+add(9,10)
+
+#subtrach multiply, divide, and finding a remainder with functions
+def  subtract(a,b):
+    print(a - b)
+
+add(9,10)
+#Division 
+def  divide(a,b):
+    print(a/b)
+
+divide(9,10)
+
+#Multiply
+def multiply(a,b):
+    print(a * b)
+
+multiply(9,10)
+
+#Remainder
+def remainder(a,b):
+    print(a %  b)
+
+remainder(9,10)
+#Exponent 
+def expontent(a,b):
+    print(a ** b)
+
+expontent(9,10)
+# y=mx+b
+def slope(a,b, c):
+    return a*b+ c
+
+print(slope(9,10, 15))
+
