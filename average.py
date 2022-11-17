3492df48e98ade580a28c562dfbe7414a1d03f3d
wowwowwes <wihezuo@gmail.com>
2022-11-17 10:24:13 -0500

    done

--- average.py
+++ average.py
@@ -1,14 +1,29 @@
 #Empty list 
-x = []
+grades = []
+#Total amount 
+total = 0
 
-grades = int(input("What is your grade?".split(" , ")))
 
-print(grades)
+# Infinte While statement is uses to repeated ask user whathtuer grade is
+while True:
+    # Simple input statement that asks  for grades in the from  of a interger 
+    x = int(input('What is your grade?'))
+    #Once those grades are in we use append to add them to our list
+    grades.append(x)
 
-sum = 0
+    z=input("Enter stop to finshing  entering  grades")
+    # I used an if statment that states if its conditions arent meet the infinte loop will broke
+    if z=="stop":
+        break
+    # If the else statments are not met then the code will just print thec grades 
+    else:
+        print(grades)
 
-for grade in x:
-    sum =  sum  + grade
+# I used a for  loop find the average of my grades
+for grade in grades:
+    total =  total + grade
 
-print(sum/len(grades))
+print('your total is...')
+#Total of my  grades is didved by  the amount of grades entered
+print (total/len(grades))
 
