d71f5ce541740701b1265a5c8a39170824d2eeb8
wowwowwes <wihezuo@gmail.com>
2022-12-08 10:13:08 -0500

    Classes

--- classes.py
+++ classes.py
@@ -15,15 +15,29 @@ class person:
         
 
 
-Wesley = person("Wesley",17)
+
+class Teacher(person): 
+    role = 'teacher'
+
+    def hi(self):
+        print("Hi  my name is Mr" + self.name)
+forlenza = Teacher("forlenza",95)
+print(forlenza.role)
+forlenza.hi()
+
+
+class student(person):
+    role = 'student'
+
+Wesley = student("Wesley",17)
+
+print(Wesley.role)
 
 print(Wesley.species)
 print(Wesley.age)
 print(Wesley.name)
 Wesley.hi()
-
 Bryant = person("Bryant",17)
 
 print(Bryant.age)
 print(Bryant.name)
-
